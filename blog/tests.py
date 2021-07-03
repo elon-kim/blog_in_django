406d59fe1028919961a38688349db663ae735299
김은종 <eunjong147@pusan.ac.kr>
2021-07-03 15:47:04 +0000

    ADD tag in text where update post

--- blog/tests.py
+++ blog/tests.py
@@ -237,14 +237,19 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
         
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+        
         response = self.client.post(
             update_post_url,
             {
                 'title' : '세 번째 포스트 수정',
                 'content' : '안녕 자기 렛미쉑댓 부리',
                 'category' : self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag',
             },
-            follow = True
+            follow = True,
         )
         
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -252,6 +257,8 @@ class TestView(TestCase):
         self.assertIn('세 번째 포스트 수정', main_area.text)
         self.assertIn('안녕 자기 렛미쉑댓 부리', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
-        
-        
+        self.assertIn('파이썬 공부', main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)
     
