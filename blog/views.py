a06efb9415847fa46e9d62b620907f65b97a74ac
김은종 <eunjong147@pusan.ac.kr>
2021-07-03 11:28:42 +0000

    ADD tag page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
@@ -39,5 +39,20 @@ def category_page(request, slug):
             'categories' : Category.objects.all(),
             'no_category_post_count' : Post.objects.filter(category=None).count(),
             'category' : category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'tag' : tag,
         }
     )
